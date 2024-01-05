0591265d53906655a0c1b40e713b157a425ffd95
Abhimanyu <abhimanyuchhoker16@gmail.com>
2024-01-05 06:55:10 +0530

    Did some coding in 'add' clause

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ print(
     "Welcome to Streak-Counter! This is a simple streak counter."
 )
 streaks = {}
+streaks_keys = list(streaks.keys())
 while True:
     print("Type 'new' to make a new streak or 'add' to increase streak: ")
     choice = str(input())
@@ -12,7 +13,11 @@ while True:
         print("New streak added.")
     elif choice.lower() == "add":
         print("Current streaks are: ")
-        for streak in streaks.keys():
+        for streak in streaks_keys:
             print(streak)
+        streak_to_be_increased = str(input("Choose a streak to be increased: "))
+        if streak_to_be_increased.lower() in map(str.lower, streaks_keys):
+            print(f"{streak_to_be_increased} will be increased.")
+            streaks[streak_to_be_increased]
         
 
